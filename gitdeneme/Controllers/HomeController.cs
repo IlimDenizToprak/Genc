aa73213de12608e17d0ac1683fcf93296e00fef3
Ilim Deniz Toprak <ilimdenizty@gmail.com>
2023-07-29 20:47:20 +0300

    first commit

---
+++ gitdeneme/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitdeneme.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Teams()
+        {
+            return View();
+        }
+        public ActionResult Teams_Detail()
+        {
+            return View();
+        }
+        public ActionResult Activities()
+        {
+            return View();
+        }
+        public ActionResult Activity_Detail()
+        {
+            return View();
+        }
+
+    }
+}
